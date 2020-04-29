b54f04dd1e1ada0633afba1920d887fe33a43f5a
Joann Pan <joannlpan@gmail.com>
2020-04-29 13:29:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,8 +6,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?
+def position_taken?(board, index)
 
+end
+
+def valid_move?(board, index)
+  index.between(0,8) && !position_taken? 
 end
 
 #turn method:
