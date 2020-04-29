bf62f63b0e16acd619c43a9561f7ef2cd6994124
Joann Pan <joannlpan@gmail.com>
2020-04-29 13:42:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,9 +19,8 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def move(board, index)
-
-
+def move(board, index, token = "X")
+  board[index] = token
 end
 
 
