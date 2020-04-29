4ce160a278dfdba4acc7afb324e7686b5774b237
Joann Pan <joannlpan@gmail.com>
2020-04-29 13:30:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between(0,8) && !position_taken?
+  index.between(0,8) && !position_taken?(board, index)
 end
 
 #turn method:
